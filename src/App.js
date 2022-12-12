2fc2e07de292e883b8258f796cf747f34e52a8bf
James Wang <90441653+Jaimou@users.noreply.github.com>
2022-12-12 11:13:15 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import Footer from "./Footer";
+import Exo from "./Exo";
+
+function App() {
+
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Exo />
+    </div>
+  );
+}
+
+export default App;
